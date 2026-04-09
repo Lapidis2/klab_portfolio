6eb292bbd4b0b6f73d43dd64083df2baf795ebad
Lapidis2 <hitlapidis2@gmail.com>
2026-04-09 10:09:44 +0200

    updating fonts

--- js/script.js
+++ js/script.js
@@ -156,3 +156,12 @@ if (heroVisual) {
     heroVisual.style.transform = "translate(0, 0)";
   });
 }
+
+
+
+ const cursor = document.querySelector(".cursor");
+
+  document.addEventListener("mousemove", (e) => {
+    cursor.style.left = e.clientX + "px";
+    cursor.style.top = e.clientY + "px";
+  });
